d256b3b44c1ba4e5ffb78d5b9ad276b3b657c65c
Zane <zanegloh@gmail.com>
2025-12-11 16:45:03 -0800

    finish part 1

--- 2025/11/11-1.c
+++ 2025/11/11-1.c
@@ -3,7 +3,108 @@
 #include <assert.h>
 #include <string.h>
 
+
+typedef struct node 
+{
+    char id[4];
+    int n_nbrs;
+    struct node **nbrs;
+} Node;
+
+int dfs(Node *n, int depth)
+{
+    if (strcmp(n->id, "out") == 0)
+        return 1;
+
+    int ans = 0;
+    for (int i = 0; i < n->n_nbrs; ++i)
+    {
+        ans += dfs(n->nbrs[i], depth+1);
+    }
+
+    return ans;
+}
+
 int main(int argc, char *argv[])
 {
+    printf("Usage: ./a.out <input>\n");
+    assert(argc == 2);
+
+    FILE *f;
+    if ((f = fopen(argc[argv-1], "r")) == NULL)
+        exit(1);
+
+    int n_lns = 604;
+    int longest_ln = 100;
+    int i = 0;
+    char lines[n_lns][longest_ln];
+
+    while (fgets(lines[i], sizeof(lines[i]), f))
+    {
+        //printf("%s", lines[i]);
+        ++i;
+    }
+    n_lns = i;
+    int n_nodes = i;
+    int you_id;
+    Node nodes[n_nodes + 1];
+
+    for (i = 0; i < n_nodes; ++i)
+    {
+        strncpy(nodes[i].id, lines[i], 3);
+        nodes[i].id[3] = '\0';
+        //printf("%s\n", nodes[i].id);
+
+        nodes[i].n_nbrs = ((strlen(lines[i]) - 1) / 4) - 1;
+        nodes[i].nbrs = (Node **)malloc(sizeof(Node *) * nodes[i].n_nbrs);
+        //printf("%d\n", nodes[i].n_nbrs);
+        if (strcmp(nodes[i].id, "you") == 0)
+            you_id = i;
+    }
+
+    // "out" node
+    strcpy(nodes[n_nodes].id, "out");
+    nodes[n_nodes].n_nbrs = 0;
+    nodes[n_nodes].nbrs = NULL;
+
+    for (i = 0; i < n_nodes; ++i)
+    {
+        char id[4];
+        for (int j = 0; j < nodes[i].n_nbrs; ++j)
+        {
+            strncpy(id, lines[i]+((j+1)*4)+1, 3);
+            id[3] = '\0';
+            if (strcmp(id, "out") == 0)
+            {
+                nodes[i].nbrs[j] = &nodes[n_nodes];
+                continue;
+            }
+            //printf("%s\n", id);
+            for (int k = 0; k < n_nodes; ++k)
+            {
+                if (strcmp(id, nodes[k].id) == 0)
+                {
+                    nodes[i].nbrs[j] = &nodes[k];
+                    break;
+                }
+            }
+        }
+    }
+
+    printf("Answer: %d\n", dfs(&nodes[you_id], 0));
+    
+    for (i = 0; i < n_nodes; ++i)
+    {
+        /*
+        printf("%s:\n", nodes[i].id);
+        for (int l = 0; l < nodes[i].n_nbrs; ++l)
+        {
+            printf("\t%s\n", nodes[i].nbrs[l]->id);
+        }
+        */
+        free(nodes[i].nbrs);
+    }
+    fclose(f);
+
     return 0;
 }
