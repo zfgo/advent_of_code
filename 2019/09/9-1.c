0ff8638e32648ac7190fb9ac4edcd41e717da4db
Zane <zanegloh@gmail.com>
2025-12-18 10:42:01 -0800

    start day 9 pt 1

--- 2019/09/9-1.c
+++ 2019/09/9-1.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <assert.h>
 
-#define PRINT       0
-#define PRINTPROG   0
-#define PRINTARGS   0
-#define PRINTIP     0
+#define PRINT       1
+#define PRINTPROG   1
+#define PRINTARGS   1
+#define PRINTIP     1
 
 /*
  * PARAMETER MODES
@@ -20,6 +21,8 @@
  *      - digits going from right to left (hundreds place, thousands place, ten 
  *        thousands place, etc.) tell us whether to use immediate mode or 
  *        position mode 
+ * Relative Mode (2):
+ *      - 
  */
 
 typedef enum op_code
@@ -32,19 +35,20 @@ typedef enum op_code
     JIF = 6,
     LT = 7,
     EQ = 8,
+    ARB = 9, 
     HALT = 99
 } OP_CODE;
 
-void print_prog(int *cmds, int n)
+void print_prog(long long *cmds, int n)
 {
 #if PRINTPROG
     for (int i = 0; i < n-1; ++i)
-        printf("%d: %d, ", i, cmds[i]);
-    printf("%d: %d\n", n-1, cmds[n-1]);
+        printf("%d: %lld, ", i, cmds[i]);
+    printf("%d: %lld\n", n-1, cmds[n-1]);
 #endif
 }
 
-void print_instr(int oc, int *cmds, int n)
+void print_instr(int oc, long long *cmds, int n)
 {
 #if PRINT
     switch (oc)
@@ -76,26 +80,29 @@ void print_instr(int oc, int *cmds, int n)
         case HALT:
             printf("HALT: ");
             break;
+        case ARB: 
+            printf("ARB: ");
+            break;
         default:
             fprintf(stderr, "Error: print_instr default case\n");
             break;
     }
     if (n == 1)
     {
-        printf("oc: %d\n", cmds[0]);
+        printf("oc: %lld\n", cmds[0]);
         return;
     }
     for (int i = 0; i < n-1; ++i)
     {
         if (i == 0)
             printf("oc: ");
-        printf("%d, ", cmds[i]);
+        printf("%lld, ", cmds[i]);
     }
-    printf("%d\n", cmds[n-1]);
+    printf("%lld\n", cmds[n-1]);
 #endif
 }
 
-void get_args(int *cmds, int ip, int *args, int n)
+void get_args(long long *cmds, int ip, long long *args, int n, long long rb)
 {
     int params = cmds[ip] / 100;
 #if PRINTARGS
@@ -103,12 +110,14 @@ void get_args(int *cmds, int ip, int *args, int n)
 #endif
     for (int i = 0; i < n; ++i)
     {
-        if (params & 1)
+        if (params % 10 == 1)       // position mode 
             args[i] = cmds[ip+i+1];
-        else
+        else if (params % 10 == 0)  // immediate mode
             args[i] = cmds[cmds[ip+i+1]];
+        else if (params % 10 == 2)  // relative mode
+            args[i] = cmds[ip+i] + rb;
 #if PRINTARGS
-        printf("%d, ", args[i]);
+        printf("%lld, ", args[i]);
 #endif
         params /= 10;
     }
@@ -117,9 +126,42 @@ void get_args(int *cmds, int ip, int *args, int n)
 #endif
 }
 
-int run(int *cmds, int n)
+void alloc_mem(long long **cmds, int n)
+{
+    *cmds = (long long *)malloc(sizeof(long long) * n);
+}
+
+void free_mem(long long *cmds)
+{
+    free(cmds);
+}
+
+// double the memory 
+void realloc_mem(long long **cmds, int *n)
+{
+    *n *= 2;
+    *cmds = (long long *)realloc(cmds, sizeof(long long) * *n);
+}
+
+void assign(long long *cmds, int addr, long long val, int *n)
+{
+    int len = *n;
+    if (addr < len)
+    {
+        cmds[addr] = val;
+        return;
+    }
+    else 
+    {
+        realloc_mem(&cmds, n);
+        assign(cmds, addr, val, n);
+    }
+}
+
+int run(long long *cmds, int n)
 {
     int ip = 0;
+    long long rb = 0;
     while (ip < n)
     {
 #if PRINTIP
@@ -130,40 +172,43 @@ int run(int *cmds, int n)
         int bufsiz = 512;   // needed for "OUT"
         char buf[bufsiz];   // needed for "OUT"
         int n_args = 8;
-        int *args = (int *)malloc(sizeof(int) * n_args);
+        long long *args = (long long *)malloc(sizeof(long long) * n_args);
 
         switch (oc)
         {
             case ADD:   // 4 params: oc, in1, in2, out
                 print_instr(oc, &cmds[ip], 4);
-                get_args(cmds, ip, args, 2);
-                cmds[cmds[ip+3]] = args[0] + args[1];
+                get_args(cmds, ip, args, 2, rb);
+                assign(cmds, cmds[ip+3], args[0]+args[1], &n);
+                //cmds[cmds[ip+3]] = args[0] + args[1];
                 ip += 4;
                 break;
             case MULT:  // 4 params: oc, in1, in2, out
                 print_instr(oc, &cmds[ip], 4);
-                get_args(cmds, ip, args, 2);
-                cmds[cmds[ip+3]] = args[0] * args[1];
+                get_args(cmds, ip, args, 2, rb);
+                assign(cmds, cmds[ip+3], args[0]*args[1], &n);
+                //cmds[cmds[ip+3]] = args[0] * args[1];
                 ip += 4;
                 break;
             case IN:    // 2 params: oc, out
                 print_instr(oc, &cmds[ip], 2);
                 if (fgets(buf, bufsiz, stdin) != NULL)
                 {
-                    sscanf(buf, "%d\n", &args[0]);
+                    sscanf(buf, "%lld\n", &args[0]);
                 }
-                cmds[cmds[ip+1]] = args[0];
+                assign(cmds, cmds[ip+1], args[0], &n);
+                //cmds[cmds[ip+1]] = args[0];
                 ip += 2;
                 break;
             case OUT:   // 2 params: oc, in
                 print_instr(oc, &cmds[ip], 2);
-                get_args(cmds, ip, args, 1);
-                printf("> OUT: %d\n", args[0]);
+                get_args(cmds, ip, args, 1, rb);
+                printf("> OUT: %lld\n", args[0]);
                 ip += 2;
                 break;
-            case JIT:   // 4 params: oc, in1, in2, out
+            case JIT:   // 3 params: oc, in1, out
                 print_instr(oc, &cmds[ip], 3);
-                get_args(cmds, ip, args, 2);
+                get_args(cmds, ip, args, 2, rb);
                 if (args[0])
                 {
                     ip = args[1];
@@ -171,9 +216,9 @@ int run(int *cmds, int n)
                 }
                 ip += 3;
                 break;
-            case JIF:
+            case JIF:   // 3 params: oc, in1, out
                 print_instr(oc, &cmds[ip], 3);
-                get_args(cmds, ip, args, 2);
+                get_args(cmds, ip, args, 2, rb);
                 if (args[0] == 0)
                 {
                     ip = args[1];
@@ -181,23 +226,41 @@ int run(int *cmds, int n)
                 }
                 ip += 3;
                 break;
-            case LT:
+            case LT:    // 4 params: oc, in1, in2, out
                 print_instr(oc, &cmds[ip], 4);
-                get_args(cmds, ip, args, 3);
+                get_args(cmds, ip, args, 3, rb);
+
                 if (args[0] < args[1])
-                    cmds[cmds[ip+3]] = 1;
+                {
+                    assign(cmds, cmds[ip+3], 1, &n);
+                    //cmds[cmds[ip+3]] = 1;
+                }
                 else 
-                    cmds[cmds[ip+3]] = 0;
+                {
+                    assign(cmds, cmds[ip+3], 0, &n);
+                    //cmds[cmds[ip+3]] = 0;
+                }
                 ip += 4;
                 break;
-            case EQ:
+            case EQ:    // 4 params: oc, in1, in2, out
                 print_instr(oc, &cmds[ip], 4);
-                get_args(cmds, ip, args, 3);
+                get_args(cmds, ip, args, 3, rb);
                 if (args[0] == args[1])
-                    cmds[cmds[ip+3]] = 1;
+                {
+                    assign(cmds, cmds[ip+3], 1, &n);
+                    //cmds[cmds[ip+3]] = 1;
+                }
                 else 
-                    cmds[cmds[ip+3]] = 0;
+                {
+                    assign(cmds, cmds[ip+3], 0, &n);
+                    //cmds[cmds[ip+3]] = 0;
+                }
                 ip += 4;
+                break;
+            case ARB:   // 2 params: oc, out
+                print_instr(oc, &cmds[ip], 2);
+                get_args(cmds, ip, args, 2, rb);
+                rb += args[0];
                 break;
             case HALT:
                 print_instr(oc, &cmds[ip], 1);
@@ -220,6 +283,7 @@ void copy_memory(int *dest, int *src, int n)
     }
 }
 
+
 int main(int argc, char *argv[])
 {
     printf("Usage: ./a.out <input>\n");
@@ -236,22 +300,25 @@ int main(int argc, char *argv[])
         ;
 
     int n = 1024;
+    long long *cmds;
+    alloc_mem(&cmds, n);
     int i = 0;
-    int init_cmds[n];
     char delim[] = ",";
     char *tok;
+    char *end;
 
     tok = strtok(buf, delim);
     while (tok != NULL)
     {
-        init_cmds[i] = atoi(tok);
+        cmds[i] = atoll(tok);
         tok = strtok(NULL, delim);
         ++i;
     }
     n = i;
 
-    run(init_cmds, n);
+    run(cmds, n);
 
+    free_mem(cmds);
     fclose(f);
     return 0;
 }
